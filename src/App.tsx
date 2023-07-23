1bdc9344224bc649ca57e7c7df13cbd1c5dce8e2
Juunmiko <estimated_pcentury@hotmail.com>
2023-07-23 14:45:42 +0700

    add p

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import About from "./about"
 
-const data : string[] = ["a", "b", "c", "z"]
+const data : string[] = ["a", "b", "c", "z", "p"]
 
 function App() {
   //const [count, setCount] = useState(0)
